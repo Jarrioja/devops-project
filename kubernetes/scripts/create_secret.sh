0650d882107a7b3b6af47fba41100266d8729b2a
Jesus <jarrioja2210@gmail.com>
2024-05-31 23:50:27 -0300

    update init scripts

--- kubernetes/scripts/create_secret.sh
+++ kubernetes/scripts/create_secret.sh
@@ -25,16 +25,23 @@ kubectl create secret generic mysql-secret \
     --from-literal=MYSQL_ROOT_PASSWORD="$MYSQL_PASSWORD" \
     --dry-run=client -o yaml | kubectl apply -f -
 
-kubectl create secret generic todo-app-secret \
+kubectl -n staging create secret generic mysql-secret \
     --from-literal=MYSQL_USER="$MYSQL_USER" \
-    --from-literal=MYSQL_DB="$MYSQL_DB" \
+    --from-literal=MYSQL_DATABASE="$MYSQL_DB" \
     --from-literal=MYSQL_PASSWORD="$MYSQL_PASSWORD" \
     --from-literal=MYSQL_ROOT_PASSWORD="$MYSQL_PASSWORD" \
-    --from-literal=MYSQL_HOST="$MYSQL_HOST" \
     --dry-run=client -o yaml | kubectl apply -f -
 
+# kubectl create secret generic todo-app-secret \
+#     --from-literal=MYSQL_USER="$MYSQL_USER" \
+#     --from-literal=MYSQL_DB="$MYSQL_DB" \
+#     --from-literal=MYSQL_PASSWORD="$MYSQL_PASSWORD" \
+#     --from-literal=MYSQL_ROOT_PASSWORD="$MYSQL_PASSWORD" \
+#     --from-literal=MYSQL_HOST="$MYSQL_HOST" \
+#     --dry-run=client -o yaml | kubectl apply -f -
+
 # Crear un secreto de Kubernetes para Grafana
-kubectl create secret generic grafana-secret \
-    -n monitoring \
-    --from-literal=admin-password="$GRAFANA_ADMIN_PASSWORD" \
-    --dry-run=client -o yaml | kubectl apply -f -
+# kubectl create secret generic grafana-secret \
+#     -n monitoring \
+#     --from-literal=admin-password="$GRAFANA_ADMIN_PASSWORD" \
+#     --dry-run=client -o yaml | kubectl apply -f -
--- kubernetes/scripts/init.sh
+++ kubernetes/scripts/init.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 chmod +x validate_env.sh create_secret.sh install_helm_charts.sh
-kubectl create namespace monitoring
-kubectl create namespace prod
-kubectl create namespace dev
+# kubectl create namespace monitoring
+kubectl create namespace staging
 kubectl create namespace argocd
 # Ejecutar el script para validar y cargar el archivo .env
 ./validate_env.sh
