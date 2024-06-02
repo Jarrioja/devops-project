bbd832d8ff63ad6fc5bbba808d53ff15764da105
Jesus <jarrioja2210@gmail.com>
2024-06-02 18:28:57 -0300

    add promethues stack

--- kubernetes/scripts/install_helm_charts.sh
+++ kubernetes/scripts/install_helm_charts.sh
@@ -5,11 +5,17 @@
 ## Añadir y actualizar repositorios de Helm
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo add argo https://argoproj.github.io/argo-helm
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add stable https://charts.helm.sh/stable
 helm repo update
 
 # Instalar ingress-nginx
 helm install nginx-ingress ingress-nginx/ingress-nginx --set controller.publishService.enabled=true
 kubectl delete validatingwebhookconfiguration nginx-ingress-ingress-nginx-admission
+
+#Instalar prometheus stack
+kubectl create namespace prometheus
+helm install prometheus-community/kube-prometheus-stack --generate-name --namespace prometheus
 
 # Instalar Argo CD
 helm install my-argo-cd argo/argo-cd --version 7.0.0 --namespace argocd
